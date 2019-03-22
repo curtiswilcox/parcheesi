1f86019ae074cd6cd4330f462d62d4c1f9745f26
Curtis Wilcox <33298869+quillcox@users.noreply.github.com>
2019-03-22 09:53:16 -0400

    Add int to accumulate lambda params

--- Player.cpp
+++ Player.cpp
@@ -13,9 +13,9 @@ Pawn::Pawn(PawnColor color) : color(color) {}
 Player::Player(QWidget *parent) : QWidget(parent) {}
 
 int Player::numPawnsStart() {
-    return accumulate(pawns.begin(), pawns.end(), 0, [&](const Pawn &pawn) { return pawn.status == PawnStatus::START; });
+    return accumulate(pawns.begin(), pawns.end(), 0, [&](int total, const Pawn &pawn) { return pawn.status == PawnStatus::START; });
 }
 
 int Player::numPawnsHome() {
-    return accumulate(pawns.begin(), pawns.end(), 0, [&](const Pawn &pawn) { return pawn.status == PawnStatus::HOME; });
+    return accumulate(pawns.begin(), pawns.end(), 0, [&](int total, const Pawn &pawn) { return pawn.status == PawnStatus::HOME; });
 }
