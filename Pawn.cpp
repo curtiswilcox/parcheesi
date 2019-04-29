cc4aaf0538a96b4c568b97afccbd8472930a917d
Curtis Wilcox <Curtis.Wilcox@uvm.edu>
2019-04-29 11:58:21 -0400

    comment out settings instantiation in mouseReleaseEvent

--- Pawn.cpp
+++ Pawn.cpp
@@ -57,7 +57,7 @@ Pawn::Pawn(const Dimensions &d, int currentTileNum, int id, string team,
 }
 
 void Pawn::mouseReleaseEvent(QMouseEvent *event) {
-    QSettings settings("CS205", "Parcheesi");
+//    QSettings settings("CS205", "Parcheesi");
 //    cout << "Pressed ";
 //    cout << team << " number " << id << endl;
     this->lambda(this);
